f8e8aa6fa9db07eb68e0e47b22158eaf97406f28
Hamza-L <hlahmimsi@gmail.com>
2024-09-03 01:28:00 -0400

    test for github auto authentification

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
 auto main(int argc, char *argv[]) -> int {
-    std::cout<<"Hello World!\n"<<std::endl;
+    std::cout<<"Hello World, this is the NanoEngine!\n"<<std::endl;
     return 0;
 }
